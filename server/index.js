9718f7b2bb7e42b06518c0f8aff0b55446012f9e
Alan <alan4tomaz8@gmail.com>
2024-04-08 22:19:18 -0300

    Commit 2 - 04/08/2024

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from './data/index.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -57,4 +60,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
     console.log("Connected to Database");
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // User.insertMany(users);
+    // Post.insertMany(posts); 
 }).catch((err) => console.log(`${err} did not connect`));
